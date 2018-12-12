6a20aee5e5a15df67c0285deeb6415d4e850adee
Kenan Hamidic <kenanhamidic@gmail.com>
2018-12-12 23:42:53 +0100

    controller done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,46 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
+    @FXML
+    public ListView list;
+    @FXML
+    public Label uzorak;
+    @FXML
+    public TextField textBox;
+    @FXML
+    public Button onFind;
+
+    private ObservableList<File> lista = FXCollections.observableArrayList();
+
+    public void initialize (URL url, ResourceBundle rb) {
+        list.setItems(lista);
+        list.getItems().addListener((ListChangeListener) (change) ->
+                list.scrollTo(list.getItems().size()-1)
+        );
+    }
+
+    @FXML
+    public void onFind(ActionEvent event) {
+        Pretraga pretraga = new Pretraga(this);
+        Thread thread = new Thread(pretraga);
+        thread.start();
+    }
+
+    public ObservableList<File> fileList() {
+        return lista;
+    }
 }
